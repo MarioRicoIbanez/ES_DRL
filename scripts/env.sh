4099658e779a547b45e6bac3a97c7aa54185ecb9
MarioRicoIbanez <marioricoibanez@epfl.ch>
2025-05-04 20:13:21 +0200

    clean up configuration files and streamline environment setup

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,7 +4,6 @@
 conda create --name mujoco_environment python=3.10 -y
 conda activate mujoco_environment
 
-pip install jax[cuda]
-pip install imageio[ffmpeg] mujoco mujoco_mjx matplotlib wandb brax joblib pre-commit
+pip install -r requirements.txt
 
 # TODO: Add torch if necessary
